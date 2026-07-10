efa0c96edf734e2af4a4fbcc30d7d1f92d01227a
sujanaankem <ankemsujana@gmail.com>
2026-07-10 22:26:48 +0530

    Objects in array fruits

--- ObjectsInArrayFruits/ObjectsInArrayFruits.js
+++ ObjectsInArrayFruits/ObjectsInArrayFruits.js
@@ -1,16 +1,29 @@
-let fruits =[];
-function onClickSubmit(){
+let fruits = [];
+function onClickSubmit() {
     debugger;
     let fruit = new Object();
     fruit.id = Number(document.getElementById("txtId").value);
+
     fruit.fruitName = document.getElementById("txtFruitName").value;
+
     fruit.category = document.getElementById("txtCategory").value;
+
     fruit.price = document.getElementById("txtPrice").value;
+
     fruit.country = document.getElementById("txtCountry").value;
+
     fruit.state = document.getElementById("txtState").value;
+
     fruit.quantity = document.getElementById("txtQuantity").value;
+
 
     fruits.push(fruit);
     document.getElementById("pResult").innerHTML = JSON.stringify(fruits);
-    
+    document.getElementById("txtId").value = "";
+    document.getElementById("txtFruitName").value = "";
+    document.getElementById("txtCategory").value = "";
+    document.getElementById("txtPrice").value = "";
+    document.getElementById("txtCountry").value = "";
+    document.getElementById("txtState").value = "";
+    document.getElementById("txtQuantity").value = "";
 }
