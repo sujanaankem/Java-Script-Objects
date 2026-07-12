e47dfe5ab33a53fe3ee8c804b728328ef4502807
sujanaankem <ankemsujana@gmail.com>
2026-07-12 23:00:44 +0530

    Objects in array fruits

--- ObjectsInArrayFruits/ObjectsInArrayFruits.js
+++ ObjectsInArrayFruits/ObjectsInArrayFruits.js
@@ -1,29 +1,26 @@
 let fruits = [];
+let user = JSON.parse(localStorage.getItem("ls_fruitShop")) || [];
 function onClickSubmit() {
     debugger;
     let fruit = new Object();
     fruit.id = Number(document.getElementById("txtId").value);
-
+    document.getElementById("txtId").value = "";
     fruit.fruitName = document.getElementById("txtFruitName").value;
-
+    document.getElementById("txtFruitName").value = "";
     fruit.category = document.getElementById("txtCategory").value;
-
+    document.getElementById("txtCategory").value = "";
     fruit.price = document.getElementById("txtPrice").value;
-
+    document.getElementById("txtPrice").value = "";
     fruit.country = document.getElementById("txtCountry").value;
-
+    document.getElementById("txtCountry").value = "";
     fruit.state = document.getElementById("txtState").value;
-
+    document.getElementById("txtState").value = "";
     fruit.quantity = document.getElementById("txtQuantity").value;
+    document.getElementById("txtQuantity").value = "";
 
 
     fruits.push(fruit);
+    localStorage.setItem("ls_fruitShop", fruits);
     document.getElementById("pResult").innerHTML = JSON.stringify(fruits);
-    document.getElementById("txtId").value = "";
-    document.getElementById("txtFruitName").value = "";
-    document.getElementById("txtCategory").value = "";
-    document.getElementById("txtPrice").value = "";
-    document.getElementById("txtCountry").value = "";
-    document.getElementById("txtState").value = "";
-    document.getElementById("txtQuantity").value = "";
+
 }
