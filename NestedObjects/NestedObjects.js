450c7f896a12cc5e0533df4ac26bfed9dd9bc9be
sujanaankem <ankemsujana@gmail.com>
2026-08-17 23:53:53 +0530

    Nested objects

---
+++ NestedObjects/NestedObjects.js
@@ -0,0 +1,53 @@
+
+var footWare = new Object();
+let array = [];
+function onClickCreate() {
+
+
+    footWare.name = document.getElementById("txtFootWareName").value;
+    footWare.color = document.getElementById("txtFootWareColor").value;
+    footWare.type = document.getElementById("txtFootWareType").value;
+    footWare.price = document.getElementById("txtFootWarePrice").value;
+    footWare.sizes = array;
+    document.getElementById("pResult").innerHTML = JSON.stringify(footWare);
+
+}
+
+function onClickAdd() {
+    debugger;
+    let footwareSize = document.getElementById("txtfootWareSize").value;
+    array.push(footwareSize);
+    document.getElementById("pResultAdd").innerHTML = `footware Size : ${array}`;
+
+}
+
+function onClickReadProperty() {
+    let property = document.getElementById("txtProperty").value;
+    document.getElementById("pResultProperty").innerHTML = footWare[property];
+
+}
+
+function onClickReadKeys() {
+    debugger;
+    for (let key in footWare) {
+        debugger;
+        alert(footWare[key]);
+    }
+
+
+} 1
+
+function onClickObjectKeys() {
+
+    var keys = Object.keys(footWare);
+    document.getElementById("pKeysResult").innerHTML = keys;
+
+}
+
+function onClickObjectValues() {
+    debugger;
+
+    var values = Object.values(footWare);
+    document.getElementById("pValuesResult").innerHTML = values;
+
+}
